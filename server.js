8aa8cf2aba6121da08c1b46ca5a0ba46131ca5e4
Shebzzzzz sayed <shehbazsayed33@gmail.com>
2021-10-27 18:25:31 +0530

    added cart & login page

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const PORT = process.env.PORT || 3000
 const expressLayout = require('express-ejs-layouts')
 const ejs = require('ejs')
 const path = require('path')
+app.use(expressLayout);
+app.set('view engine','ejs');
+app.set('views', path.join(__dirname,'/resources/views'))
 
 app.use(express.static('public'))
 
@@ -11,11 +14,21 @@ app.get('/', (req,res) => {
     res.render('home')
 })
 
+app.get('/cart',(req,res) => {
+    res.render('customers/cart')
+})
+
+app.get('/login',(req,res) => {
+    res.render('auth/login')
+})
+
+app.get('/register',(req,res) => {
+    res.render('auth/register')
+})
 
 
-app.use(expressLayout);
-app.set('view engine','ejs');
-app.set('views', path.join(__dirname,'/resources/views'))
+
+
 
 
 app.listen(PORT,() => {
